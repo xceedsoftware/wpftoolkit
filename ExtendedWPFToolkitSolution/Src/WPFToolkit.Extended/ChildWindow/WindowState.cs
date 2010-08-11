73ae2b52baf36fdaa57c66c72df62cefe204b34e
brianlagunas_cp <XceedBoucherS@users.noreply.github.com>
2010-08-11 00:03:11 +0000

    restricted ChildWindow in container. fixed bug with ChildWindow zindex, added a WindowState property to ChildWindow which will control opening and closing the window through data binding; two states WindowState.Open and WindowState.Closed.  Added OverlayBrush and Overlay Opacity properties allow users to modify the ChldWindow's OverLay element.

---
+++ ExtendedWPFToolkitSolution/Src/WPFToolkit.Extended/ChildWindow/WindowState.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace Microsoft.Windows.Controls
+{
+    public enum WindowState
+    {
+        Closed,
+        Open
+    }
+}
