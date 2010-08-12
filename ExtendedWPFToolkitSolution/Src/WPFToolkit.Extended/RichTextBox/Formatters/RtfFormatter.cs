c014a69dc762c7800deb355c423c97dcd023d4aa
brianlagunas_cp <XceedBoucherS@users.noreply.github.com>
2010-08-12 01:34:57 +0000

    added new RichTextBox control.  Extends the Windows.Controls.RichTextbox to include a Text dependency property that supports data binding.  This control uses the concept of text formatters. TextFormatters allow the user to format the text in the RichTextBox into any format they wish.  Three text formatters are included: PlainTextFormatter, RtfFormatter, and a XamlFormatter.  User can create their own custom formatters by creating a class that inherits from ITextFormatter and setting that as the default formatter in the <RichTextBox.TextFormatter> block.

---
+++ ExtendedWPFToolkitSolution/Src/WPFToolkit.Extended/RichTextBox/Formatters/RtfFormatter.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Text;
+using System.Windows.Documents;
+using System.IO;
+using System.Windows;
+
+namespace Microsoft.Windows.Controls
+{
+    /// <summary>
+    /// Formats the RichTextBox text as RTF
+    /// </summary>
+    public class RtfFormatter : ITextFormatter
+    {
+        public string GetText(FlowDocument document)
+        {
+            TextRange tr = new TextRange(document.ContentStart, document.ContentEnd);
+            using (MemoryStream ms = new MemoryStream())
+            {
+                tr.Save(ms, DataFormats.Rtf);
+                return ASCIIEncoding.Default.GetString(ms.ToArray());
+            }
+        }
+
+        public void SetText(FlowDocument document, string text)
+        {
+            TextRange tr = new TextRange(document.ContentStart, document.ContentEnd);
+            using (MemoryStream ms = new MemoryStream(Encoding.ASCII.GetBytes(text)))
+            {
+                tr.Load(ms, DataFormats.Rtf);
+            }
+        }
+    }
+}
