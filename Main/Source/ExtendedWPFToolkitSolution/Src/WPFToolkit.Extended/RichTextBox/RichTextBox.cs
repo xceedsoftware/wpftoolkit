0d9e713b3291f24ed5e004b0af943587356960a1
brianlagunas_cp <SND\brianlagunas_cp>
2010-08-12 01:34:57 +0000

    added new RichTextBox control.  Extends the Windows.Controls.RichTextbox to include a Text dependency property that supports data binding.  This control uses the concept of text formatters. TextFormatters allow the user to format the text in the RichTextBox into any format they wish.  Three text formatters are included: PlainTextFormatter, RtfFormatter, and a XamlFormatter.  User can create their own custom formatters by creating a class that inherits from ITextFormatter and setting that as the default formatter in the <RichTextBox.TextFormatter> block.

---
+++ Main/Source/ExtendedWPFToolkitSolution/Src/WPFToolkit.Extended/RichTextBox/RichTextBox.cs
@@ -0,0 +1,115 @@
+﻿using System;
+using System.Windows;
+using System.Windows.Data;
+
+namespace Microsoft.Windows.Controls
+{
+    public class RichTextBox : System.Windows.Controls.RichTextBox
+    {
+        #region Private Members
+
+        private bool _textHasLoaded;
+
+        #endregion //Private Members
+
+        #region Constructors
+
+        public RichTextBox()
+        {
+            Loaded += RichTextBox_Loaded;
+        }
+
+        public RichTextBox(System.Windows.Documents.FlowDocument document)
+            : base(document)
+        {
+            
+        }  
+       
+        #endregion //Constructors
+
+        #region Properties
+
+        private ITextFormatter _textFormatter;
+        /// <summary>
+        /// The ITextFormatter the is used to format the text of the RichTextBox.
+        /// Deafult formatter is the RtfFormatter
+        /// </summary>
+        public ITextFormatter TextFormatter
+        {
+            get
+            {
+                if (_textFormatter == null)
+                    _textFormatter = new RtfFormatter(); //default is rtf
+
+                return _textFormatter;
+            }
+            set
+            {
+                _textFormatter = value;
+            }
+        }
+
+        #region Text
+
+        public static readonly DependencyProperty TextProperty = DependencyProperty.Register("Text", typeof(string), typeof(RichTextBox), new FrameworkPropertyMetadata(String.Empty, FrameworkPropertyMetadataOptions.BindsTwoWayByDefault, new PropertyChangedCallback(OnTextPropertyChanged), new CoerceValueCallback(CoerceTextProperty), true, System.Windows.Data.UpdateSourceTrigger.LostFocus));
+        public string Text
+        {
+            get { return (string)GetValue(TextProperty); }
+            set { SetValue(TextProperty, value); }
+        }
+
+        private static void OnTextPropertyChanged(DependencyObject d, DependencyPropertyChangedEventArgs e)
+        {
+            RichTextBox rtb = (RichTextBox)d;
+
+            if (!rtb._textHasLoaded)
+            {
+                rtb.TextFormatter.SetText(rtb.Document, (string)e.NewValue);
+                rtb._textHasLoaded = true;
+            }
+        }
+
+        private static object CoerceTextProperty(DependencyObject d, object value)
+        {
+            return value ?? "";
+        }
+
+        #endregion //Text
+
+        #endregion //Properties
+
+        #region Methods
+
+        private void UpdateText()
+        {
+            //when the Text is null and the Text hasn't been loaded, it indicates that the OnTextPropertyChanged event hasn't exceuted
+            //and since we are initializing the text from here, we don't want the OnTextPropertyChanged to execute, so set the loaded flag to true.
+            //this prevents the cursor to jumping to the front of the textbox after the first letter is typed.
+            if (!_textHasLoaded && string.IsNullOrEmpty(Text))
+                _textHasLoaded = true;
+
+            if (_textHasLoaded)
+                Text = TextFormatter.GetText(Document);
+        }
+
+        #endregion //Methods
+
+        #region Event Hanlders
+
+        private void RichTextBox_Loaded(object sender, RoutedEventArgs e)
+        {
+            Binding binding = BindingOperations.GetBinding(this, TextProperty);
+
+            if (binding.UpdateSourceTrigger == UpdateSourceTrigger.Default || binding.UpdateSourceTrigger == UpdateSourceTrigger.LostFocus)
+            {
+                LostFocus += (o, ea) => UpdateText();
+            }
+            else
+            {
+                TextChanged += (o, ea) => UpdateText();
+            }
+        }
+
+        #endregion //Event Hanlders
+    }
+}
