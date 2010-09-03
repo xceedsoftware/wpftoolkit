8ce6714ab51f6f796cf76d03d4d5b86d298cf1ae
brianlagunas_cp <XceedBoucherS@users.noreply.github.com>
2010-09-03 01:07:35 +0000

    added Extended WPF Toolkit solution for Visual Studio 2008 and .NET 3.5 - this has a dependency on the WPFToolkit

---
+++ ExtendedWPFToolkitSolution_35/Src/WPFToolkit.Extended/RichTextBox/Formatters/XamlFormatter.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Text;
+using System.IO;
+using System.Windows.Documents;
+using System.Windows;
+
+namespace Microsoft.Windows.Controls
+{
+    /// <summary>
+    /// Formats the RichTextBox text as Xaml
+    /// </summary>
+    public class XamlFormatter : ITextFormatter
+    {
+        public string GetText(System.Windows.Documents.FlowDocument document)
+        {
+            TextRange tr = new TextRange(document.ContentStart, document.ContentEnd);
+            using (MemoryStream ms = new MemoryStream())
+            {
+                tr.Save(ms, DataFormats.Xaml);
+                return ASCIIEncoding.Default.GetString(ms.ToArray());
+            }
+        }
+
+        public void SetText(System.Windows.Documents.FlowDocument document, string text)
+        {
+            try
+            {
+                TextRange tr = new TextRange(document.ContentStart, document.ContentEnd);
+                using (MemoryStream ms = new MemoryStream(Encoding.ASCII.GetBytes(text)))
+                {
+                    tr.Load(ms, DataFormats.Xaml);
+                }
+            }
+            catch
+            {
+                throw new InvalidDataException("data provided is not in the correct Xaml format.");
+            }
+        }
+    }
+}
