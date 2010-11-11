cf4a8d4201c31ca669121747c4fa592a66089187
brianlagunas_cp <XceedBoucherS@users.noreply.github.com>
2010-11-11 00:27:25 +0000

    added new DateTimeUpDown control.  Aloows you to use spin buttons or the mouse wheel to increment/decrement individual sections of a DateTime value.  There are currently 10 supported formats: FullDateTime, LongDate, LongTime, MonthDay, RFC1123, ShortDate, ShortTime, SortableDateTime, UniversalSortableDateTime, and YearMonth.  Use the control as follows:
    <extToolkit:DateTimeUpDown Value="{Binding LastUpdated}" Format="FullDateTime" />
    
    You cannot type in the Value yet because I haven't figured that part out yet.

---
+++ ExtendedWPFToolkitSolution/Src/WPFToolkit.Extended/DateTimeUpDown/DateTimeFormat.cs
@@ -0,0 +1,18 @@
+﻿using System;
+
+namespace Microsoft.Windows.Controls
+{
+    public enum DateTimeFormat
+    {
+        FullDateTime,
+        LongDate,
+        LongTime,
+        MonthDay,
+        RFC1123,
+        ShortDate,
+        ShortTime,
+        SortableDateTime,
+        UniversalSortableDateTime,
+        YearMonth
+    }
+}
---
+++ ExtendedWPFToolkitSolution/Src/WPFToolkit.Extended/DateTimeUpDown/DateTimePart.cs
@@ -0,0 +1,22 @@
+using System;
+
+namespace Microsoft.Windows.Controls
+{
+    public enum DateTimePart
+    {
+        Day,
+        DayName,
+        AmPmDesignator,
+        Millisecond,
+        Hour12,
+        Hour24,
+        Minute,
+        Month,
+        MonthName,
+        Other,
+        Period,
+        TimeZone,
+        Second,
+        Year
+    }
+}
