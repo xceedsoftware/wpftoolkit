bc471ac16cd2bacb13516ec32cd83a2532be4987
brianlagunas_cp <SND\brianlagunas_cp>
2010-11-11 16:48:26 +0000

    did some refactoring of DateTimeUpDown

--- Main/Source/ExtendedWPFToolkitSolution/Src/WPFToolkit.Extended/DateTimeUpDown/DateTimeInfo.cs
+++ Main/Source/ExtendedWPFToolkitSolution/Src/WPFToolkit.Extended/DateTimeUpDown/DateTimeInfo.cs
@@ -5,11 +5,9 @@ namespace Microsoft.Windows.Controls
     internal class DateTimeInfo
     {
         public string Content { get; set; }
-        public bool? IsReadOnly { get; set; }
-        public int KeyPressCount { get; set; }
+        public string Format { get; set; }
+        public bool IsReadOnly { get; set; }
         public int Length { get; set; }
-        public string MonthName { get; set; }
-        public string Pattern { get; set; }
         public int StartPosition { get; set; }
         public DateTimePart Type { get; set; }
     }
