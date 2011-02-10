a3503467007375bffb830956b8dc4f2adfc7b29d
brianlagunas_cp <SND\brianlagunas_cp>
2011-02-10 01:10:45 +0000

    ColorPicker: added new property that allows you to specify how to show the color information.  You can show just the color or the color and color name.  Just set the DisplayColorAndName property to true to display the color block and the name of the color next to it.
    <extToolkit:ColorPicker DisplayColorAndName="True"/>

--- Main/Source/ExtendedWPFToolkitSolution/Src/WPFToolkit.Extended/ColorPicker/ColorUtilities.cs
+++ Main/Source/ExtendedWPFToolkitSolution/Src/WPFToolkit.Extended/ColorPicker/ColorUtilities.cs
@@ -8,12 +8,14 @@ namespace Microsoft.Windows.Controls
 {
     static class ColorUtilities
     {
+        public static readonly Dictionary<string, Color> KnownColors = GetKnownColors();
+
         public static string GetColorName(this Color color)
         {
-            return _knownColors.Where(kvp => kvp.Value.Equals(color)).Select(kvp => kvp.Key).FirstOrDefault();
+            return KnownColors.Where(kvp => kvp.Value.Equals(color)).Select(kvp => kvp.Key).FirstOrDefault();
         }
-        static readonly Dictionary<string, Color> _knownColors = GetKnownColors();
-        static Dictionary<string, Color> GetKnownColors()
+
+        private static Dictionary<string, Color> GetKnownColors()
         {
             var colorProperties = typeof(Colors).GetProperties(BindingFlags.Static | BindingFlags.Public);
             return colorProperties.ToDictionary(p => p.Name, p => (Color)p.GetValue(null, null));
