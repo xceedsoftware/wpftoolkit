68a8143c63a1ce759a3ec7bc9351a2fefee27a56
brianlagunas_cp <XceedBoucherS@users.noreply.github.com>
2011-03-12 21:35:42 +0000

    PropertyGrid: implemented SelectedProperty.

--- ExtendedWPFToolkitSolution/Src/WPFToolkit.Extended/PropertyGrid/Implementation/PropertyItem.cs
+++ ExtendedWPFToolkitSolution/Src/WPFToolkit.Extended/PropertyGrid/Implementation/PropertyItem.cs
@@ -7,102 +7,64 @@ namespace Microsoft.Windows.Controls.PropertyGrid
 {
     public class PropertyItem : Control
     {
-        #region Members
-
-        public object Instance { get; private set; }
-        public PropertyDescriptor PropertyDescriptor { get; private set; }
-
-        #endregion //Members
-
         #region Properties
 
-        public string Description
-        {
-            get { return PropertyDescriptor.Description; }
-        }
+        #region Category
 
-        public bool IsWriteable
-        {
-            get { return !IsReadOnly; }
-        }
-
-        public bool IsReadOnly
-        {
-            get { return PropertyDescriptor.IsReadOnly; }
-        }
-
-        public Type PropertyType
-        {
-            get { return PropertyDescriptor.PropertyType; }
-        }
-
-        //public string Category
-        //{
-        //    get { return PropertyDescriptor.Category; }
-        //}
-
-        public static readonly DependencyProperty CategoryProperty = DependencyProperty.Register("Category", typeof(string), typeof(PropertyItem), new UIPropertyMetadata(string.Empty, new PropertyChangedCallback(OnCategoryChanged), new CoerceValueCallback(OnCoerceCategory)));
-
-        private static object OnCoerceCategory(DependencyObject o, object value)
-        {
-            PropertyItem propertyItem = o as PropertyItem;
-            if (propertyItem != null)
-                return propertyItem.OnCoerceCategory((string)value);
-            else
-                return value;
-        }
-
-        private static void OnCategoryChanged(DependencyObject o, DependencyPropertyChangedEventArgs e)
-        {
-            PropertyItem propertyItem = o as PropertyItem;
-            if (propertyItem != null)
-                propertyItem.OnCategoryChanged((string)e.OldValue, (string)e.NewValue);
-        }
-
-        protected virtual string OnCoerceCategory(string value)
-        {
-            // TODO: Keep the proposed value within the desired range.
-            return value;
-        }
-
-        protected virtual void OnCategoryChanged(string oldValue, string newValue)
-        {
-            // TODO: Add your property changed side-effects. Descendants can override as well.
-        }
-
+        public static readonly DependencyProperty CategoryProperty = DependencyProperty.Register("Category", typeof(string), typeof(PropertyItem), new UIPropertyMetadata(string.Empty));
         public string Category
         {
-            // IMPORTANT: To maintain parity between setting a property in XAML and procedural code, do not touch the getter and setter inside this dependency property!
-            get
-            {
-                return (string)GetValue(CategoryProperty);
-            }
-            set
-            {
-                SetValue(CategoryProperty, value);
-            }
+            get { return (string)GetValue(CategoryProperty); }
+            set { SetValue(CategoryProperty, value); }
         }
 
+        #endregion //Category
+
+        public string Description { get { return PropertyDescriptor.Description; } }
+
+        public object Instance { get; private set; }
+
+        public bool IsReadOnly { get { return PropertyDescriptor.IsReadOnly; } }
+
+        #region IsSelected
+
+        public static readonly DependencyProperty IsSelectedProperty = DependencyProperty.Register("IsSelected", typeof(bool), typeof(PropertyItem), new UIPropertyMetadata(false, OnIsSelectedChanged));
+        public bool IsSelected
+        {
+            get { return (bool)GetValue(IsSelectedProperty); }
+            set { SetValue(IsSelectedProperty, value); }
+        }
+
+        private static void OnIsSelectedChanged(DependencyObject o, DependencyPropertyChangedEventArgs e)
+        {
+            PropertyItem propertyItem = o as PropertyItem;
+            if (propertyItem != null)
+                propertyItem.OnIsSelectedChanged((bool)e.OldValue, (bool)e.NewValue);
+        }
+
+        protected virtual void OnIsSelectedChanged(bool oldValue, bool newValue)
+        {
+            if (newValue)
+                PropertyGrid.SelectedProperty = this;
+        }
+
+        #endregion //IsSelected
+
+        public bool IsWriteable { get { return !IsReadOnly; } }
+
+        public PropertyDescriptor PropertyDescriptor { get; private set; }
+
+        public PropertyGrid PropertyGrid { get; private set; }
+
+        public Type PropertyType { get { return PropertyDescriptor.PropertyType; } }
 
         #region Editor
 
-        public static readonly DependencyProperty EditorProperty = DependencyProperty.Register("Editor", typeof(FrameworkElement), typeof(PropertyItem), new UIPropertyMetadata(null, OnEditorChanged));
+        public static readonly DependencyProperty EditorProperty = DependencyProperty.Register("Editor", typeof(FrameworkElement), typeof(PropertyItem), new UIPropertyMetadata(null));
         public FrameworkElement Editor
         {
             get { return (FrameworkElement)GetValue(EditorProperty); }
             set { SetValue(EditorProperty, value); }
-        }
-
-        private static void OnEditorChanged(DependencyObject o, DependencyPropertyChangedEventArgs e)
-        {
-            PropertyItem propertyItem = o as PropertyItem;
-            if (propertyItem != null)
-                propertyItem.OnEditorChanged((FrameworkElement)e.OldValue, (FrameworkElement)e.NewValue);
-        }
-
-        protected virtual void OnEditorChanged(FrameworkElement oldValue, FrameworkElement newValue)
-        {
-            // TODO: Add your property changed side-effects. Descendants can override as well.
         }
 
         #endregion //Editor
@@ -116,25 +78,24 @@ namespace Microsoft.Windows.Controls.PropertyGrid
             DefaultStyleKeyProperty.OverrideMetadata(typeof(PropertyItem), new FrameworkPropertyMetadata(typeof(PropertyItem)));
         }
 
-        public PropertyItem(object instance, PropertyDescriptor property)
+        public PropertyItem(object instance, PropertyDescriptor property, PropertyGrid propertyGrid)
         {
             Instance = instance;
             PropertyDescriptor = property;
             Name = PropertyDescriptor.Name;
             Category = PropertyDescriptor.Category;
+            PropertyGrid = propertyGrid;
         }
 
         #endregion //Constructor
 
-        #region INotifyPropertyChanged Members
+        #region Base Class Overrides
 
-        public event System.ComponentModel.PropertyChangedEventHandler PropertyChanged;
-        protected void OnPropertyChanged(string propertyName)
+        protected override void OnPreviewMouseLeftButtonDown(System.Windows.Input.MouseButtonEventArgs e)
         {
-            if (PropertyChanged != null)
-                PropertyChanged(this, new System.ComponentModel.PropertyChangedEventArgs(propertyName));
+            IsSelected = true;
         }
 
-        #endregion
+        #endregion //Base Class Overrides
     }
 }
