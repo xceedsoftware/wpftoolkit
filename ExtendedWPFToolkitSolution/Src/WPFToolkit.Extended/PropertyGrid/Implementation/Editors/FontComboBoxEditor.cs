ae7a34850e803611fd380153eaec62a7fee8a393
brianlagunas_cp <XceedBoucherS@users.noreply.github.com>
2011-03-14 00:51:40 +0000

    PropertyGrid: FontComboBoxEditor implemented all fonts.

--- ExtendedWPFToolkitSolution/Src/WPFToolkit.Extended/PropertyGrid/Implementation/Editors/FontComboBoxEditor.cs
+++ ExtendedWPFToolkitSolution/Src/WPFToolkit.Extended/PropertyGrid/Implementation/Editors/FontComboBoxEditor.cs
@@ -1,4 +1,5 @@
-﻿using System.Collections.Generic;
+﻿using System.Linq;
+using System.Collections.Generic;
 using System.Windows;
 using System.Windows.Media;
 
@@ -9,54 +10,20 @@ namespace Microsoft.Windows.Controls.PropertyGrid.Editors
         protected override IList<object> CreateItemsSource(PropertyItem propertyItem)
         {
             if (propertyItem.PropertyType == typeof(FontFamily))
-            {
                 return GetFontFamilies();
-            }
             else if (propertyItem.PropertyType == typeof(FontWeight))
-            {
                 return GetFontWeights();
-            }
             else if (propertyItem.PropertyType == typeof(FontStyle))
-            {
                 return GetFontStyles();
-            }
             else if (propertyItem.PropertyType == typeof(FontStretch))
-            {
                 return GetFontStretches();
-            }
 
             return null;
         }
 
         private static IList<object> GetFontFamilies()
         {
-            IList<object> fontFamilies = new List<object>();
-
-            //TODO: get all fonts
-            fontFamilies.Add(new FontFamily("Arial"));
-            fontFamilies.Add(new FontFamily("Courier New"));
-            fontFamilies.Add(new FontFamily("Times New Roman"));
-            fontFamilies.Add(new FontFamily("Batang"));
-            fontFamilies.Add(new FontFamily("BatangChe"));
-            fontFamilies.Add(new FontFamily("DFKai-SB"));
-            fontFamilies.Add(new FontFamily("Dotum"));
-            fontFamilies.Add(new FontFamily("DutumChe"));
-            fontFamilies.Add(new FontFamily("FangSong"));
-            fontFamilies.Add(new FontFamily("GulimChe"));
-            fontFamilies.Add(new FontFamily("Gungsuh"));
-            fontFamilies.Add(new FontFamily("GungsuhChe"));
-            fontFamilies.Add(new FontFamily("KaiTi"));
-            fontFamilies.Add(new FontFamily("Malgun Gothic"));
-            fontFamilies.Add(new FontFamily("Meiryo"));
-            fontFamilies.Add(new FontFamily("Microsoft JhengHei"));
-            fontFamilies.Add(new FontFamily("Microsoft YaHei"));
-            fontFamilies.Add(new FontFamily("MingLiU"));
-            fontFamilies.Add(new FontFamily("MingLiu_HKSCS"));
-            fontFamilies.Add(new FontFamily("MingLiu_HKSCS-ExtB"));
-            fontFamilies.Add(new FontFamily("MingLiu-ExtB"));
-            fontFamilies.Add(new FontFamily("Segoe UI"));
-
-            return fontFamilies;
+            return Fonts.SystemFontFamilies.ToList<object>();
         }
 
         private static IList<object> GetFontWeights()
