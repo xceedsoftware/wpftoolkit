35907b1ecc5c94096cf94c41434535fc0bddcc31
brianlagunas_cp <XceedBoucherS@users.noreply.github.com>
2011-03-21 16:23:04 +0000

    DateTimePicker: minor fix with Calendar not releasing the mouse capture.

--- ExtendedWPFToolkitSolution/Src/WPFToolkit.Extended/DateTimePicker/Implementation/DateTimePicker.cs
+++ ExtendedWPFToolkitSolution/Src/WPFToolkit.Extended/DateTimePicker/Implementation/DateTimePicker.cs
@@ -1,16 +1,8 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
 using System.Windows;
 using System.Windows.Controls;
-using System.Windows.Data;
-using System.Windows.Documents;
 using System.Windows.Input;
-using System.Windows.Media;
-using System.Windows.Media.Imaging;
-using System.Windows.Navigation;
-using System.Windows.Shapes;
+using System.Windows.Controls.Primitives;
 
 namespace Microsoft.Windows.Controls
 {
@@ -147,6 +139,12 @@ namespace Microsoft.Windows.Controls
             _calendar = (Calendar)GetTemplateChild("Part_Calendar");
             _calendar.SelectedDatesChanged += Calendar_SelectedDatesChanged;
             _calendar.SelectedDate = SelectedDate;
+        }
+
+        protected override void OnPreviewMouseUp(MouseButtonEventArgs e)
+        {
+            if (Mouse.Captured is CalendarItem) 
+                Mouse.Capture(null);  
         }
 
         #endregion //Base Class Overrides
