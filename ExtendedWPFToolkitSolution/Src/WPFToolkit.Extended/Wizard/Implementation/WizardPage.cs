817c85bd615d083c3bd29c10e5d9564e2c85f805
brianlagunas_cp <XceedBoucherS@users.noreply.github.com>
2011-07-29 03:26:37 +0000

    initial checkin of Wizard control and sample module.  Nothing usable yet, just mainly stubbing out the code.

---
+++ ExtendedWPFToolkitSolution/Src/WPFToolkit.Extended/Wizard/Implementation/WizardPage.cs
@@ -0,0 +1,57 @@
+﻿using System;
+using System.Windows;
+using System.Windows.Controls;
+
+namespace Microsoft.Windows.Controls
+{
+    public class WizardPage : ContentControl
+    {
+        #region Properties
+
+        public static readonly DependencyProperty CaptionProperty = DependencyProperty.Register("Caption", typeof(string), typeof(WizardPage));
+        public string Caption
+        {
+            get { return (string)base.GetValue(CaptionProperty); }
+            set { base.SetValue(CaptionProperty, value); }
+        }
+
+        public static readonly DependencyProperty DescriptionProperty = DependencyProperty.Register("Description", typeof(string), typeof(WizardPage));
+        public string Description
+        {
+            get { return (string)base.GetValue(DescriptionProperty); }
+            set { base.SetValue(DescriptionProperty, value); }
+        }
+
+        public static readonly DependencyProperty NextPageProperty = DependencyProperty.Register("NextPage", typeof(WizardPage), typeof(WizardPage), new UIPropertyMetadata(null));
+        public WizardPage NextPage
+        {
+            get { return (WizardPage)GetValue(NextPageProperty); }
+            set { SetValue(NextPageProperty, value); }
+        }
+
+        public static readonly DependencyProperty PreviousPageProperty = DependencyProperty.Register("PreviousPage", typeof(WizardPage), typeof(WizardPage), new UIPropertyMetadata(null));
+        public WizardPage PreviousPage
+        {
+            get { return (WizardPage)GetValue(PreviousPageProperty); }
+            set { SetValue(PreviousPageProperty, value); }
+        }        
+
+        public static readonly DependencyProperty TitleProperty = DependencyProperty.Register("Title", typeof(string), typeof(WizardPage));
+        public string Title
+        {
+            get { return (string)base.GetValue(TitleProperty); }
+            set { base.SetValue(TitleProperty, value); }
+        }
+
+        #endregion //Properties
+
+        #region Constructors
+
+        static WizardPage()
+        {
+            DefaultStyleKeyProperty.OverrideMetadata(typeof(WizardPage), new FrameworkPropertyMetadata(typeof(WizardPage)));
+        }
+
+        #endregion //Constructors
+    }
+}
