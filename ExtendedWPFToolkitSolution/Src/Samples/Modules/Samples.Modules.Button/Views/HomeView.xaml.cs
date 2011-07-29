54fdc2c07201945d3712347c2e0c1a830cb258a3
brianlagunas_cp <XceedBoucherS@users.noreply.github.com>
2011-07-29 22:46:26 +0000

    Wizard: Implemented Wizard Pages, Help, Back, Next buttons.  Added features to set visibility of buttons from the Wizard and then override the behavior from the individual wizard pages. No styling yet.  It's pretty plain.

--- ExtendedWPFToolkitSolution/Src/Samples/Modules/Samples.Modules.Button/Views/HomeView.xaml.cs
+++ ExtendedWPFToolkitSolution/Src/Samples/Modules/Samples.Modules.Button/Views/HomeView.xaml.cs
@@ -11,6 +11,8 @@ using System.Windows.Media;
 using System.Windows.Media.Imaging;
 using System.Windows.Navigation;
 using System.Windows.Shapes;
+using System.ComponentModel;
+using System.Collections.ObjectModel;
 
 namespace Samples.Modules.Button.Views
 {
@@ -22,6 +24,102 @@ namespace Samples.Modules.Button.Views
         public HomeView()
         {
             InitializeComponent();
+            DataContext = new MyViewModel();
+        }
+    }
+
+    public class Item
+    {
+        public bool IsChecked { get; set; }
+        public string Name { get; set; }
+
+        public Item()
+        {
+            
+        }
+    }
+    public class MyViewModel : INotifyPropertyChanged
+    {
+        public ICommand MyCommand { get; private set; }
+
+        private int _clickCount;
+        public int ClickCount
+        {
+            get { return _clickCount; }
+            set
+            {
+                _clickCount = value;
+                OnPropertyChanged("ClickCount");
+            }
+        }
+
+        private ObservableCollection<Item> _items;
+        public ObservableCollection<Item> Items
+        {
+            get { return _items; }
+            set
+            {
+                _items = value;
+                OnPropertyChanged("Items");
+            }
+        }
+        
+
+        public MyViewModel()
+        {
+            MyCommand = new CustomCommand(Execute, CanExecute);
+
+            Items = new ObservableCollection<Item>();
+            for (int i = 0; i < 10; i++)
+            {
+                Items.Add(new Item() { IsChecked = i % 2 == 0, Name = String.Format("Item {0}", i) });
+            }
+        }
+
+        private void Execute(object param)
+        {
+            ClickCount++;
+            //MessageBox.Show(String.Format("Executed {0}", param));
+        }
+
+        private bool CanExecute(object param)
+        {
+            return Convert.ToInt32(param) != 5;
+        }
+
+        public event PropertyChangedEventHandler PropertyChanged;
+        protected void OnPropertyChanged(string propertyName)
+        {
+            if (PropertyChanged != null)
+                PropertyChanged(this, new PropertyChangedEventArgs(propertyName));
+        }
+    }
+
+    public class CustomCommand : ICommand
+    {
+        Action<object> _execute;
+        Func<object, bool> _canExecute;
+
+        public CustomCommand(Action<object> execute, Func<object, bool> canExecute)
+        {
+            _execute = execute;
+            _canExecute = canExecute;
+        }
+
+        public bool CanExecute(object parameter)
+        {
+            return _canExecute.Invoke(parameter);
+        }
+
+        public event EventHandler CanExecuteChanged
+        {
+            add { CommandManager.RequerySuggested += value; }
+            remove { CommandManager.RequerySuggested -= value; }
+        }
+
+        public void Execute(object parameter)
+        {
+            _execute(parameter);
         }
     }
 }
---
+++ ExtendedWPFToolkitSolution/Src/WPFToolkit.Extended/Wizard/Implementation/WizardPageButtonVisibility.cs
@@ -0,0 +1,12 @@
+using System;
+
+namespace Microsoft.Windows.Controls
+{
+    public enum WizardPageButtonVisibility
+    {
+        Inherit,
+        Collapsed,
+        Hidden,
+        Visible
+    }
+}
