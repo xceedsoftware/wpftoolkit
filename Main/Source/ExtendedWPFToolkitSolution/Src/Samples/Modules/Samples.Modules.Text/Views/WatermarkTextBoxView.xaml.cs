0510faf29124666fa26fde7eae7df67da96abd6e
brianlagunas_cp <SND\brianlagunas_cp>
2011-08-05 04:47:41 +0000

    added WatermakrTextBox samples to Sample application.

---
+++ Main/Source/ExtendedWPFToolkitSolution/Src/Samples/Modules/Samples.Modules.Text/Views/WatermarkTextBoxView.xaml.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using Samples.Infrastructure.Controls;
+using Microsoft.Practices.Prism.Regions;
+
+namespace Samples.Modules.Text.Views
+{
+    /// <summary>
+    /// Interaction logic for WatermarkTextBoxView.xaml
+    /// </summary>
+    [RegionMemberLifetime(KeepAlive=false)]
+    public partial class WatermarkTextBoxView : DemoView
+    {
+        public WatermarkTextBoxView()
+        {
+            InitializeComponent();
+        }
+    }
+}
