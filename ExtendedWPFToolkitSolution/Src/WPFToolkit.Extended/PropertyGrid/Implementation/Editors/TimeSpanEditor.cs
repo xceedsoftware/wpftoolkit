3fbc48411a24d39f954bf2603007998e35fd3652
brianlagunas_cp <XceedBoucherS@users.noreply.github.com>
2011-09-26 20:33:16 +0000

    PropertyGrid: added TimeSpan editor

---
+++ ExtendedWPFToolkitSolution/Src/WPFToolkit.Extended/PropertyGrid/Implementation/Editors/TimeSpanEditor.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Windows.Data;
+using System.Globalization;
+
+namespace Microsoft.Windows.Controls.PropertyGrid.Editors
+{
+    public class TimeSpanEditor : DateTimeUpDownEditor
+    {
+        private sealed class TimeSpanConverter : IValueConverter
+        {
+            object IValueConverter.Convert(object value, Type targetType, object parameter, CultureInfo culture)
+            {
+                return DateTime.Today + (TimeSpan)value;
+            }
+
+            object IValueConverter.ConvertBack(object value, Type targetType, object parameter, CultureInfo culture)
+            {
+                return ((DateTime)value).TimeOfDay;
+            }
+        }
+
+        protected override void SetControlProperties()
+        {
+            Editor.Format = DateTimeFormat.LongTime;
+        }
+
+        protected override IValueConverter CreateValueConverter()
+        {
+            return new TimeSpanConverter();            
+        }
+    }
+}
