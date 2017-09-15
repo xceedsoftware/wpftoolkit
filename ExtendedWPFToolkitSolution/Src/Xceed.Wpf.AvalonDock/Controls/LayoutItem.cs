09260f4406f5d2989011eae0f35d29126cc6fb76
BoucherS <BoucherS@xceed.com>
2017-09-15 07:39:37 -0400

    Update Pull Request 1170 to not use C# 6 stuff.
    Update Pull Request 1170 to not use C# 6 stuff.

--- ExtendedWPFToolkitSolution/Src/Xceed.Wpf.AvalonDock/Controls/LayoutItem.cs
+++ ExtendedWPFToolkitSolution/Src/Xceed.Wpf.AvalonDock/Controls/LayoutItem.cs
@@ -204,9 +204,15 @@ namespace Xceed.Wpf.AvalonDock.Controls
                     _view = new ContentPresenter();
 
                     _view.SetBinding(ContentPresenter.ContentProperty, new Binding("Content") { Source = LayoutElement });
-                    _view.SetBinding( ContentPresenter.ContentTemplateProperty, new Binding( "LayoutItemTemplate" ) { Source = LayoutElement.Root.Manager } );
-                    _view.SetBinding( ContentPresenter.ContentTemplateSelectorProperty, new Binding( "LayoutItemTemplateSelector" ) { Source = LayoutElement.Root.Manager } );
-                    LayoutElement.Root.Manager.InternalAddLogicalChild( _view );
+                    if ((LayoutElement != null) && (LayoutElement.Root != null))
+                    {
+                        _view.SetBinding(ContentPresenter.ContentTemplateProperty, new Binding("LayoutItemTemplate") { Source = LayoutElement.Root.Manager });
+                        _view.SetBinding(ContentPresenter.ContentTemplateSelectorProperty, new Binding("LayoutItemTemplateSelector") { Source = LayoutElement.Root.Manager });
+                        if (LayoutElement.Root.Manager != null)
+                        {
+                            LayoutElement.Root.Manager.InternalAddLogicalChild(_view);
+                        }
+                    }
                 }
 
                 return _view;
