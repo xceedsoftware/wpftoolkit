92cf03c9e7b740de02cec5db42ab35a83d07ba65
AlexejLiebenthal <alexej.liebenthal@gmail.com>
2018-02-03 18:18:11 +0100

    Added some unittests for the HtmlFormatter

---
+++ ExtendedWPFToolkitSolution/Xceed.Wpf.Toolkit.Tests/RichTextBox/Formatters/HtmlFormatterTests.cs
@@ -0,0 +1,71 @@
+﻿using System.Diagnostics;
+using System.IO;
+using System.Security.Cryptography.X509Certificates;
+using System.Text;
+using System.Windows;
+using NUnit.Framework;
+using System.Windows.Documents;
+using System.Xml.Linq;
+
+namespace Xceed.Wpf.Toolkit.Tests
+{
+    [TestFixture]
+    public class HtmlFormatterTests
+    {
+        public HtmlFormatter CreateInstance() => new HtmlFormatter();
+
+        public FlowDocument CreateSimpleFlowDocument()
+        {
+            var r = new Run("Hallo Welt!");
+            var b = new Bold(r);
+            var p = new Paragraph(b);
+            return  new FlowDocument(p);
+        }
+
+        public string CreateSimpleHtml() =>
+            "<html><body><p><b>Hallo Welt!</b></p><body></html>";
+
+        [Test]
+        public void GetText_ShouldReturnAHtmlText()
+        {
+            var sut = CreateInstance();
+            var doc = CreateSimpleFlowDocument();
+
+            var actual = sut.GetText(doc);
+
+            const string expectedText = "Hallo Welt!";
+            const string expectedBoldStyle = "bold";
+            const string expectedHtmlTag = "<html>";
+
+            Assert.Multiple(delegate
+            {
+                Assert.That(actual, Does.Contain(expectedText));
+                Assert.That(actual, Does.Contain(expectedBoldStyle));
+                Assert.That(actual, Does.Contain(expectedHtmlTag));
+            });
+        }
+
+        [Test]
+        public void SetText_ShouldSetAHtmlTextToAFlowDocument()
+        {
+            var sut = CreateInstance();
+            var doc = new FlowDocument();
+            var text = CreateSimpleHtml();
+
+            sut.SetText(doc, text);
+
+            var actual = new XamlFormatter().GetText(doc);
+
+            const string expectedText = "Hallo Welt!";
+            const string expectedBoldStyle = "Bold";
+            const string expectedSection = "Section";
+
+            Assert.Multiple(() =>
+            {
+                Assert.That(actual, Does.Contain(expectedText));
+                Assert.That(actual, Does.Contain(expectedBoldStyle));
+                Assert.That(actual, Does.Contain(expectedSection));
+            });
+        }
+    }
+}
