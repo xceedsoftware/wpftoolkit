cac3192ad6b01c5e1d54231122592e6fd83846ce
Christopher Brown <christopher.brown@apdcomms.com>
2019-02-15 11:46:31 +0000

    Removed DataGrid nuspec as no longer part of the free version of Toolkit after 3.5

--- ExtendedWPFToolkitSolution/Src/Xceed.Wpf.Toolkit/CollectionControl/Implementation/CollectionControlDialog.xaml.cs
+++ ExtendedWPFToolkitSolution/Src/Xceed.Wpf.Toolkit/CollectionControl/Implementation/CollectionControlDialog.xaml.cs
@@ -117,7 +117,7 @@ namespace Xceed.Wpf.Toolkit
     {
       get
       {
-        return _localCollectionControl;
+        return _collectionControl;
       }
     }
 
@@ -175,14 +175,14 @@ namespace Xceed.Wpf.Toolkit
         }
       }
 
-      _localCollectionControl.PersistChanges();
+      _collectionControl.PersistChanges();
       this.DialogResult = true;
       this.Close();
     }
 
     private void CancelButton_Click( object sender, RoutedEventArgs e )
     {
-      _localCollectionControl.PersistChanges( originalData );
+      _collectionControl.PersistChanges( originalData );
       this.DialogResult = false;
       this.Close();
     }
@@ -329,7 +329,7 @@ namespace Xceed.Wpf.Toolkit
 
     private bool AreDictionaryKeysValid()
     {
-      var keys = _localCollectionControl.Items.Select( x =>
+      var keys = _collectionControl.Items.Select( x =>
       {
         var keyType = x.GetType().GetProperty( "Key" );
         if( keyType != null )
@@ -339,7 +339,7 @@ namespace Xceed.Wpf.Toolkit
         return null;
       } );
 
-      return ( keys.Distinct().Count() == _localCollectionControl.Items.Count )
+      return ( keys.Distinct().Count() == _collectionControl.Items.Count )
              && keys.All( x => x != null );
     }
 
