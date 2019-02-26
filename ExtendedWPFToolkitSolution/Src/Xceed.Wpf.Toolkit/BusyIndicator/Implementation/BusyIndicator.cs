aa93c693a581c6e97b7cec0c0d3c43faca4d1380
BoucherS <BoucherS@xceed.com>
2019-02-26 09:06:42 -0500

    V3.5.0 Reviewed

--- ExtendedWPFToolkitSolution/Src/Xceed.Wpf.Toolkit/BusyIndicator/Implementation/BusyIndicator.cs
+++ ExtendedWPFToolkitSolution/Src/Xceed.Wpf.Toolkit/BusyIndicator/Implementation/BusyIndicator.cs
@@ -14,7 +14,7 @@
 
   ***********************************************************************************/
 
-using System;
+using System; 
 using System.Windows;
 using System.Windows.Controls;
 using System.Windows.Shapes;
