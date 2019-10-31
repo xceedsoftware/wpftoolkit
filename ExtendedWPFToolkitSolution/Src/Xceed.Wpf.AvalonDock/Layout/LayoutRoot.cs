78674d35b4628ed2ce3fa584fdcc8d0a2452d52a
Paul Martin <paul.martin@cosworth.com>
2019-10-31 17:57:55 +0000

    Revert whitespace changes in file header.

--- ExtendedWPFToolkitSolution/Src/Xceed.Wpf.AvalonDock/Layout/LayoutRoot.cs
+++ ExtendedWPFToolkitSolution/Src/Xceed.Wpf.AvalonDock/Layout/LayoutRoot.cs
@@ -1,11 +1,11 @@
 ﻿/*************************************************************************************
-
+   
    Toolkit for WPF
 
    Copyright (C) 2007-2018 Xceed Software Inc.
 
    This program is provided to you under the terms of the Microsoft Public
-   License (Ms-PL) as published at http://wpftoolkit.codeplex.com/license
+   License (Ms-PL) as published at http://wpftoolkit.codeplex.com/license 
 
    For more features, controls, and fast professional support,
    pick up the Plus Edition at https://xceed.com/xceed-toolkit-plus-for-wpf/
