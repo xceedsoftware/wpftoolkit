c80d2f58e605485c3c5a9b3731cdcc10a101f4f5
Pat Limosnero <pat@renewedvision.com>
2024-08-07 10:58:29 -0400

    upgrade Xceed.Wpf.DataGrid
    fix a couple of issues and some commented. we don't really care much about this project since we dont use it in propresenter

--- ExtendedWPFToolkitSolution/Src/Xceed.Wpf.DataGrid/Converters/ImageConverter.cs
+++ ExtendedWPFToolkitSolution/Src/Xceed.Wpf.DataGrid/Converters/ImageConverter.cs
@@ -15,96 +15,92 @@
   ***********************************************************************************/
 
 using System;
-using System.Collections.Generic;
-using System.Text;
-using System.Windows.Data;
-using System.Windows.Controls;
-using System.Windows.Media;
-using System.Windows.Media.Imaging;
-using System.Windows;
 using System.ComponentModel;
+using System.Windows;
+using System.Windows.Data;
+using System.Windows.Media;
 
 namespace Xceed.Wpf.DataGrid.Converters
 {
-  [EditorBrowsable( EditorBrowsableState.Never )]
-  [ValueConversion( typeof( object ), typeof( ImageSource ) )]
-  public class ImageConverter : IValueConverter
-  {
-    #region IValueConverter Members
-
-    [System.Diagnostics.CodeAnalysis.SuppressMessage( "Microsoft.Performance", "CA1800:DoNotCastUnnecessarily" )]
-    public object Convert( object value, Type targetType, object parameter, System.Globalization.CultureInfo culture )
+    [EditorBrowsable(EditorBrowsableState.Never)]
+    [ValueConversion(typeof(object), typeof(ImageSource))]
+    public class ImageConverter : IValueConverter
     {
-      if( !targetType.IsAssignableFrom( typeof( ImageSource ) ) )
-        return DependencyProperty.UnsetValue;
+        #region IValueConverter Members
 
-      if( ( value == null ) || ( value is ImageSource ) )
-        return value;
-
-      ImageSource imageSource = null;
-
-      System.Drawing.Image image = value as System.Drawing.Image;
-
-      if( image != null )
-      {
-        imageSource = ImageConverter.ConvertFromWinFormsImage( image );
-      }
-      else
-      {
-        byte[] byteArray = value as byte[];
-
-        if( byteArray != null )
+        [System.Diagnostics.CodeAnalysis.SuppressMessage("Microsoft.Performance", "CA1800:DoNotCastUnnecessarily")]
+        public object Convert(object value, Type targetType, object parameter, System.Globalization.CultureInfo culture)
         {
-          imageSource = ImageConverter.ConvertFromByteArray( byteArray );
+            if (!targetType.IsAssignableFrom(typeof(ImageSource)))
+                return DependencyProperty.UnsetValue;
+
+            if ((value == null) || (value is ImageSource))
+                return value;
+
+            ImageSource imageSource = null;
+
+            //System.Drawing.Image image = value as System.Drawing.Image;
+
+            //if (image != null)
+            //{
+            //    imageSource = ImageConverter.ConvertFromWinFormsImage(image);
+            //}
+            //else
+            //{
+            byte[] byteArray = value as byte[];
+
+            if (byteArray != null)
+            {
+                imageSource = ImageConverter.ConvertFromByteArray(byteArray);
+            }
+            //}
+
+            return imageSource;
         }
-      }
 
-      return imageSource;
+        public object ConvertBack(object value, Type targetType, object parameter, System.Globalization.CultureInfo culture)
+        {
+            return Binding.DoNothing;
+        }
+
+        #endregion
+
+        #region PRIVATE METHODS
+
+        //private static ImageSource ConvertFromWinFormsImage(System.Drawing.Image image)
+        //{
+        //    ImageSource imageSource = null;
+
+        //    try
+        //    {
+        //        System.Drawing.ImageConverter imageConverter = new System.Drawing.ImageConverter();
+
+        //        byte[] imageBytes = (byte[])imageConverter.ConvertTo(image, typeof(byte[]));
+
+        //        if (imageBytes != null)
+        //            imageSource = ImageConverter.ConvertFromByteArray(imageBytes);
+        //    }
+        //    catch { }
+
+        //    return imageSource;
+        //}
+
+        private static ImageSource ConvertFromByteArray(byte[] imageBytes)
+        {
+            ImageSource imageSource = null;
+
+            try
+            {
+                ImageSourceConverter imageSourceConverter = new ImageSourceConverter();
+                imageSource = imageSourceConverter.ConvertFrom(imageBytes) as ImageSource;
+            }
+            catch
+            {
+            }
+
+            return imageSource;
+        }
+
+        #endregion PRIVATE METHODS
     }
-
-    public object ConvertBack( object value, Type targetType, object parameter, System.Globalization.CultureInfo culture )
-    {
-      return Binding.DoNothing;
-    }
-
-    #endregion
-
-    #region PRIVATE METHODS
-
-    private static ImageSource ConvertFromWinFormsImage( System.Drawing.Image image )
-    {
-      ImageSource imageSource = null;
-
-      try
-      {
-        System.Drawing.ImageConverter imageConverter = new System.Drawing.ImageConverter();
-
-        byte[] imageBytes = ( byte[] )imageConverter.ConvertTo( image, typeof( byte[] ) );
-
-        if( imageBytes != null )
-          imageSource = ImageConverter.ConvertFromByteArray( imageBytes );
-      }
-      catch {}
-
-      return imageSource;
-    }
-
-    private static ImageSource ConvertFromByteArray( byte[] imageBytes )
-    {
-      ImageSource imageSource = null;
-
-      try
-      {
-        ImageSourceConverter imageSourceConverter = new ImageSourceConverter();
-        imageSource = imageSourceConverter.ConvertFrom( imageBytes ) as ImageSource;
-      }
-      catch
-      {
-      }
-
-      return imageSource;
-    }
-
-    #endregion PRIVATE METHODS
-  }
 }
